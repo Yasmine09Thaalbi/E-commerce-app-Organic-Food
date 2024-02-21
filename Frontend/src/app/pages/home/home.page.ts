b77b56d21b60e7a7f43303a0e80bf62831c0e20e
Yasmine <ythaalbi@gmail.com>
2024-02-21 10:05:16 +0100

    Home page

--- Frontend/src/app/pages/home/home.page.ts
+++ Frontend/src/app/pages/home/home.page.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+CartPage() {
+throw new Error('Method not implemented.');
+}
+addToCart() {
+throw new Error('Method not implemented.');
+}
+goToAccountPage() {
+throw new Error('Method not implemented.');
+}
+goToHomePage() {
+throw new Error('Method not implemented.');
+}
+goBack() {
+throw new Error('Method not implemented.');
+}
+  goToCartPage() {
+
+  }
 
   constructor() {}
 
