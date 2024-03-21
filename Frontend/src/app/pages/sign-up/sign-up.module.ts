89b864b513ef83effb388a550f23565aa8943e01
benabdallahemna <amnabenabdallah2001@email.com>
2024-03-21 11:38:53 +0100

    Backend sign up and sign in

--- Frontend/src/app/pages/sign-up/sign-up.module.ts
+++ Frontend/src/app/pages/sign-up/sign-up.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
@@ -13,7 +14,9 @@ import { SignUpPage } from './sign-up.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SignUpPageRoutingModule
+    SignUpPageRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [SignUpPage]
 })
